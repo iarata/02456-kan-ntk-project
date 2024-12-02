a59d69e831cbf86364611b8da3c47d16cc83c644
dragonoverlord3000 <hugomn2002@gmail.com>
2024-12-02 12:06:40 +0100

    Omniglot

--- introduction_code.py
+++ introduction_code.py
@@ -14,12 +14,48 @@ from tqdm import tqdm
 import random
 
 def pkernel_loss(y_pred: torch.Tensor, loss: torch.nn.Module) -> torch.Tensor:
+    """
+    Computes the loss function with one-hot y_preds and normalizes by number of classes,
+    pkernel computes the loss over all possible targets, as opposed to over e.g. the actual
+    targets, this is helpful for forming the ntk matrix.
+
+    Args:
+        y_pred: the predictions of the model i.e. model(x), [N_samples, C] for C classes
+        loss: the loss function we use
+    """
+    # Variable to hold the loss
     total_loss = 0.
+    # The batch size
     B = y_pred.shape[0]
+    # The number of classes in the dataset
     n_classes = y_pred.shape[-1]
+    # For each class, compute the loss and add it to total loss
     for i in range(n_classes):
+        """
+        sps i = 2, n = 5 and C = 3, then the following line can be broken down as follows
+
+        torch.zeros_like(y_pred) --->
+        [[0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0]]
+        torch.tensor([i], dtype=torch.long, device=y_pred.device) --->
+        [2]
+        .repeat(B)
+        [2, 2, 2]
+        .unsqueeze(1)
+        [[2], [2], [2]]
+        scatter(1, ..., 1)
+        [[0, 0, 1, 0, 0],
+         [0, 0, 1, 0, 0],
+         [0, 0, 1, 0, 0]]
+        """
         y_target = torch.zeros_like(y_pred).scatter(1, torch.tensor([i], dtype=torch.long, device=y_pred.device).repeat(B).unsqueeze(1), 1)
+
+        # Some loss function that works on one-hot encoded targets
         total_loss += loss(y_pred.float(), y_target)
+
+    # Divide loss by sqrt(n_classes), this is just for stabization and bcs it appears in theory
+    # In theory, dividing by # classes should ofc not do anything to the optimum or the gradient direction
     total_loss = total_loss / n_classes**0.5
     return total_loss
 
@@ -39,11 +75,14 @@ def get_gradient(model: torch.nn.Module, x: torch.Tensor, loss_fn: callable, opt
     Returns:
         grads: the gradients of the model with respect to the loss function
     """
-    B = len(x)
+    # Number of samples / Batch size
+    B = len(x) 
+    # GPU or CPU
     device = next(model.parameters()).device
     x = x.to(device)
     if y is not None:
         y = y.to(device).float()
+    # Zero any existing gradients, then compute predictions on the input data
     opt.zero_grad()
     y_pred = model(x)
     if use_label:
@@ -51,7 +90,8 @@ def get_gradient(model: torch.nn.Module, x: torch.Tensor, loss_fn: callable, opt
     else:
         y_target = y_pred.detach().argmax(1)
         y_target = torch.zeros_like(y_pred).scatter(1, y_target.unsqueeze(1), 1)
-       
+    
+    # Compute the kernel_loss based on the loss function used
     loss = pkernel_loss(y_pred, loss_fn)
     # Trick to get gradient with respect to each sample in parallel
     grads = torch.autograd.grad(loss, model.parameters(), is_grads_batched=True, grad_outputs=torch.eye(B).to(device))
@@ -198,45 +238,45 @@ def sample_data(n: int, treshold: float = 0.5**0.5, seed: Optional[int] = None, 
     return x, y
 
 
-def plot_decision_boundary(model: torch.nn.Module, treshold: float, fig, ax, title = None) -> None:
-    model.eval()
-    y_hat = torch.stack([model.forward(x_test_batch) for x_test_batch in torch.split(x_test, 100)]).view(-1)
-    y_acc = (y_hat >= 0.5)== ((x_test[:, 0] > treshold) | (x_test[:, 1] > treshold))
-    ax.clear()
+# def plot_decision_boundary(model: torch.nn.Module, treshold: float, fig, ax, title = None) -> None:
+#     model.eval()
+#     y_hat = torch.stack([model.forward(x_test_batch) for x_test_batch in torch.split(x_test, 100)]).view(-1)
+#     y_acc = (y_hat >= 0.5)== ((x_test[:, 0] > treshold) | (x_test[:, 1] > treshold))
+#     ax.clear()
     
-    y_hat_reshape = y_hat.view(int(N_testing**0.5), int(N_testing**0.5)).detach().numpy().reshape(int(N_testing**0.5), int(N_testing**0.5))
-    # Plot decision boundary
-    # Color blue if y_hat > 0.5, red if y_hat < 0.5 by making a colormap of two colors
-    colors = ["tab:red", "tab:blue"]
-    custom_cmap = plt.cm.colors.ListedColormap(colors)
-    cb = ax.contourf(x0, x1, y_hat_reshape >0.5, alpha=0.5, levels=torch.linspace(-5.5, 5.5, 3), cmap=custom_cmap)
+#     y_hat_reshape = y_hat.view(int(N_testing**0.5), int(N_testing**0.5)).detach().numpy().reshape(int(N_testing**0.5), int(N_testing**0.5))
+#     # Plot decision boundary
+#     # Color blue if y_hat > 0.5, red if y_hat < 0.5 by making a colormap of two colors
+#     colors = ["tab:red", "tab:blue"]
+#     custom_cmap = plt.cm.colors.ListedColormap(colors)
+#     cb = ax.contourf(x0, x1, y_hat_reshape >0.5, alpha=0.5, levels=torch.linspace(-5.5, 5.5, 3), cmap=custom_cmap)
      
-    # Plot training data
-    ax.scatter(x_train[y_train == 0, 0], x_train[y_train == 0, 1], c='r', label='Class 0', s=4, marker='x')
-    ax.scatter(x_train[y_train == 1, 0], x_train[y_train == 1, 1], c='b', label='Class 1', s=4, marker='x')
-    # Add title
-    if title is not None:
-        ax.set_title(title)
-    else:
-        ax.set_title('Accuracy: {:.2f}'.format(y_acc.float().mean().item()))
-    ax.set_xlabel('$x_0$')
-    ax.set_ylabel('$x_1$')
+#     # Plot training data
+#     ax.scatter(x_train[y_train == 0, 0], x_train[y_train == 0, 1], c='r', label='Class 0', s=4, marker='x')
+#     ax.scatter(x_train[y_train == 1, 0], x_train[y_train == 1, 1], c='b', label='Class 1', s=4, marker='x')
+#     # Add title
+#     if title is not None:
+#         ax.set_title(title)
+#     else:
+#         ax.set_title('Accuracy: {:.2f}'.format(y_acc.float().mean().item()))
+#     ax.set_xlabel('$x_0$')
+#     ax.set_ylabel('$x_1$')
     
-    # Add striped line to mark the decision boundary (x0 < treshold and x1 < treshold)
-    ax.plot([0, treshold], [treshold, treshold], 'k--')
-    ax.axvline(x=treshold, color='k', linestyle='--', ymax=treshold, label='Correct Decision boundary')
-    ax.legend(loc='upper center')
-    model.train()
+#     # Add striped line to mark the decision boundary (x0 < treshold and x1 < treshold)
+#     ax.plot([0, treshold], [treshold, treshold], 'k--')
+#     ax.axvline(x=treshold, color='k', linestyle='--', ymax=treshold, label='Correct Decision boundary')
+#     ax.legend(loc='upper center')
+#     model.train()
     
-def plot_NTK_decision_boundary(n_hidden: int = 64, noise_var: float = 0.0, ax: Optional[plt.Axes] = None, fig: Optional[plt.Figure] = None, title = None):
-    n_hidden = int(n_hidden)
-    model = train_model  # model_arch(2, 1, n_hidden)
+# def plot_NTK_decision_boundary(n_hidden: int = 64, noise_var: float = 0.0, ax: Optional[plt.Axes] = None, fig: Optional[plt.Figure] = None, title = None):
+#     n_hidden = int(n_hidden)
+#     model = train_model  # model_arch(2, 1, n_hidden)
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.02, momentum=0.9)
-    kernel_model = GaussianFit(model, "cpu", noise_var=noise_var)
-    kernel_model.fit(gradient_loader, optimizer, MSELoss_batch)
-    plot_decision_boundary(kernel_model, treshold, fig, ax, title)
-    plt.show()
+#     optimizer = torch.optim.SGD(model.parameters(), lr=0.02, momentum=0.9)
+#     kernel_model = GaussianFit(model, "cpu", noise_var=noise_var)
+#     kernel_model.fit(gradient_loader, optimizer, MSELoss_batch)
+#     plot_decision_boundary(kernel_model, treshold, fig, ax, title)
+#     plt.show()
 
 
 # ##### START OF DEMO #####
